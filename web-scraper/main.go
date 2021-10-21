fbe6c73309f37fe9ee380e95b7e07fe18381475f
Yusuf İşleyen <me@yusufisleyen.com>
2021-10-21 23:10:01 +0300

    Html parse işlemi eklendi

--- web-scraper/main.go
+++ web-scraper/main.go
@@ -1,1 +1,21 @@
-package web_scraper
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+func main() {
+
+	doc, err := goquery.NewDocument("http://www.borakasmer.com/category/code-first/")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	doc.Find(".container .post-row").Each(func(i int, s *goquery.Selection) {
+		title := s.Find(".post-title").Text()
+		fmt.Println(title)
+	})
+}
