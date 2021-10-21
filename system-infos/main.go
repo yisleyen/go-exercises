6688477e9c89cf673635f242ff3dc9af4ca5f38e
Yusuf İşleyen <me@yusufisleyen.com>
2021-10-21 23:08:27 +0300

    Sistem bilgilerini öğrenme işlemi eklendi

--- system-infos/main.go
+++ system-infos/main.go
@@ -1,1 +1,23 @@
-package system_infos
+package main
+
+import (
+	"fmt"
+
+	"github.com/shirou/gopsutil/cpu"
+	"github.com/shirou/gopsutil/disk"
+	"github.com/shirou/gopsutil/host"
+	"github.com/shirou/gopsutil/mem"
+)
+
+func main() {
+	hostStat, _ := host.Info()
+	cpuStat, _ := cpu.Info()
+	vmStat, _ := mem.VirtualMemory()
+	diskStat, _ := disk.Usage("\\") // If you're in Unix change this "\\" for "/"
+
+	fmt.Println("Bilgisayar Adı:", hostStat.Hostname)
+	fmt.Println("İşletim Sistemi:", hostStat.Platform)
+	fmt.Println("İşlemci:", cpuStat[0].ModelName)
+	fmt.Println("RAM:", vmStat.Total/1024/1024)
+	fmt.Println("Disk:", diskStat.Total/1024/1024)
+}
