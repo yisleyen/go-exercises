7be1ce1126d39c2c1e60eefc869ec18521c42d49
Yusuf İşleyen <me@yusufisleyen.com>
2021-10-21 23:09:14 +0300

    Waitgroup eklendi

--- waitgroup/main.go
+++ waitgroup/main.go
@@ -1,1 +1,29 @@
-package waitgroup
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main()  {
+	var wg sync.WaitGroup
+
+	fmt.Println("app started")
+	wg.Add(2)
+
+	go func1(&wg)
+	go func2(&wg)
+
+	wg.Wait()
+	fmt.Println("app finished")
+}
+
+func func1(wg *sync.WaitGroup)  {
+	fmt.Println("function 1")
+	wg.Done()
+}
+
+func func2(wg *sync.WaitGroup)  {
+	fmt.Println("function 2")
+	wg.Done()
+}
