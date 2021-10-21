b04875307931170a8d3fba7d4d7208ad0e86df02
Yusuf İşleyen <me@yusufisleyen.com>
2021-10-21 23:07:39 +0300

    Json ayrıştırma işlemi eklendi

--- json-parser/main.go
+++ json-parser/main.go
@@ -1,1 +1,39 @@
-package json_parser
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type Todos struct {
+	UserId    int    `json:"userId"`
+	Id        int    `json:"id"`
+	Title     string `json:"title"`
+	Completed bool   `json:"completed"`
+}
+
+func main() {
+	resp, err := http.Get("https://jsonplaceholder.typicode.com/todos")
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		fmt.Println("Error: ", err)
+	}
+
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
+
+	var todos []Todos
+	json.Unmarshal(data, &todos)
+
+	for i := range todos {
+		fmt.Printf("Title: %s \n", todos[i].Title)
+	}
+}
