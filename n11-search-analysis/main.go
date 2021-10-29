c2e082fdf221cbc614f3bc99303a476fd53af2da
Yusuf İşleyen <me@yusufisleyen.com>
2021-10-30 01:01:57 +0300

    n11 arama sayfası analiz eklendi

---
+++ n11-search-analysis/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+)
+
+type Product struct {
+	title string
+	subTitle string
+}
+
+type Products struct {
+	products  []Product
+}
+
+func main()  {
+	doc, err := goquery.NewDocument("https://www.n11.com/arama?q=iphone+13+k%C4%B1l%C4%B1f")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	var product Product
+
+	doc.Find(".column").Each(func(i int, s *goquery.Selection) {
+
+		product.title = s.Find(".productName").Text()
+		product.subTitle = s.Find(".proSubTitle").Text()
+
+		fmt.Println(product.title)
+	})
+}
